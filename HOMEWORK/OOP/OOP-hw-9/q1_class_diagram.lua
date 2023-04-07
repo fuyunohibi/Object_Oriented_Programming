f80a8ee5e15c669077102758358a7ca00a2a8778
Ko-Kwan Mongkholtham <122147145+fuyunohibi@users.noreply.github.com>
2023-04-07 15:59:58 +0700

    Class Diagram

---
+++ HOMEWORK/OOP/OOP-hw-9/q1_class_diagram.lua
@@ -0,0 +1,27 @@
+                        +-----------------+
+                        |     Writer      |
+                        +-----------------+
+                        | write(str: string) |
+                        +-----------------+
+                                 ^
+                                 |
+                                 |
+                        +-----------------+
+                        |    HTMLWriter   |
+                        +-----------------+
+                        | write(str: string) |
+                        +-----------------+
+
+                        +-----------------+
+                        |   Doc_element   |
+                        +-----------------+
+                        | name_: string   |
+                        | children_: vector<Doc_element> |
+                        +-----------------+
+                        | write_document(w: Writer) const |
+                        | write_to(w: Writer, lv: int) const |
+                        +-----------------+
+                        | + text(t: string) : Doc_element |
+                        | + Doc_element(n: string) |
+                        | + Doc_element(n: string, children: vector<Doc_element>) |
+                        +-----------------+
